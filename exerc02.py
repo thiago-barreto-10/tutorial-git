89a6688e1caf00012c32f36a55bce6b3b942d167
thiago barreto <thiagoj1231@outlook.com>
2022-11-06 17:40:10 -0300

    commit inicial

---
+++ exerc02.py
@@ -0,0 +1,9 @@
+
+genero = input('qual o seu gênero? ')
+
+if genero =='masculino' or genero == 'm':
+    print('Sexo masculino!')
+elif genero == 'feminino' or genero == 'f' :
+    print('Sexo feminino!')
+else:
+    print('sexo invalido!')
